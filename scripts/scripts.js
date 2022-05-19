dca499b2b7ae80103ae922013bf1c173912ccd31
tingjc <jean.claude.ting@gmail.com>
2022-05-19 12:43:07 -0400

    added overflow scroll to projects section. mobile nav now dismisses on link click

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,32 +1,46 @@
 const app = {}
 
 // menu button stuff
+app.checkBox = document.querySelector("header input");
 app.menuButton = document.querySelector("header i");
-app.menuButton.addEventListener("click", (event) => {
+app.menuButton.addEventListener("click", () => {
     app.menuCall();
-    app.menuButton.classList.toggle('fa-bars')
-    app.menuButton.classList.toggle('fa-xmark')
-
 })
 
 //When menu button is clicked
     // change header-ul to "top: 0"
     // change i to "x"
 app.menuCall = () => {
-    const checkBox = document.querySelector("header input");
-    const ul = document.querySelector("header ul");
-
-    if (checkBox.checked) {
-        ul.classList.toggle('upTop');
+    if (app.checkBox.checked) {
+        app.upTopToggle()
+        app.menuButton.classList.toggle('fa-bars');
+        app.menuButton.classList.toggle('fa-xmark');
         
     } else {
-        ul.classList.toggle('upTop');
+        app.upTopToggle();
+        app.menuButton.classList.toggle('fa-bars');
+        app.menuButton.classList.toggle('fa-xmark');
     }
+}
+
+app.upTopToggle = () => {
+    const ul = document.querySelector("header ul");
+    ul.classList.toggle('upTop');
+}
+
+//add functions to links in nav to dismiss the nav
+app.dismissHeader = () => {
+    const anchor = document.querySelectorAll("header nav ul a");
+    anchor.forEach((link) => {
+        link.addEventListener("click", () => {
+            app.menuCall();
+        })
+    })
 }
 
 app.init = () => {
     console.log("hello");
-    
+    app.dismissHeader();
 
 }
 
